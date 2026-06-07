e88baa582af1982aad4d5219d08ee8d2878ebe5a
Jaden Stanton <jaden.stanton6@gmail.com>
2026-06-07 12:36:00 -0500

    Update config

--- init.lua
+++ init.lua
@@ -199,118 +199,57 @@ do
   -- Enable faster startup by caching compiled Lua modules
   vim.loader.enable()
 
-  -- Set <space> as the leader key
-  -- See `:help mapleader`
-  --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
   vim.g.mapleader = ' '
   vim.g.maplocalleader = ' '
 
   -- Set to true if you have a Nerd Font installed and selected in the terminal
   vim.g.have_nerd_font = true
 
-  -- [[ Setting options ]]
-  --  See `:help vim.o`
-  -- NOTE: You can change these options as you wish!
-  --  For more options, you can see `:help option-list`
-
-  -- Make line numbers default
   vim.o.number = true
-  -- You can also add relative line numbers, to help with jumping.
-  --  Experiment for yourself to see if you like it!
-  -- vim.o.relativenumber = true
-
-  -- Enable mouse mode, can be useful for resizing splits for example!
   vim.o.mouse = 'a'
-
-  -- Don't show the mode, since it's already in the status line
   vim.o.showmode = false
 
-  -- Sync clipboard between OS and Neovim.
-  --  Schedule the setting after `UiEnter` because it can increase startup-time.
-  --  Remove this option if you want your OS clipboard to remain independent.
-  --  See `:help 'clipboard'`
   vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
 
-  -- Enable break indent
   vim.o.breakindent = true
   vim.o.tabstop = 4
   vim.o.softtabstop = 4
   vim.o.shiftwidth = 4
   vim.o.expandtab = true
 
-  -- Enable undo/redo changes even after closing and reopening a file
   vim.o.undofile = true
-
-  -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
   vim.o.ignorecase = true
   vim.o.smartcase = true
-
-  -- Keep signcolumn on by default
   vim.o.signcolumn = 'yes'
-
-  -- Decrease update time
   vim.o.updatetime = 250
-
-  -- Decrease mapped sequence wait time
   vim.o.timeoutlen = 300
-
-  -- Configure how new splits should be opened
   vim.o.splitright = true
   vim.o.splitbelow = true
-
-  -- Sets how neovim will display certain whitespace characters in the editor.
-  --  See `:help 'list'`
-  --  and `:help 'listchars'`
-  --
-  --  Notice listchars is set using `vim.opt` instead of `vim.o`.
-  --  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
-  --   See `:help lua-options`
-  --   and `:help lua-guide-options`
   vim.o.list = true
   vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
-  -- Preview substitutions live, as you type!
   vim.o.inccommand = 'split'
-
-  -- Show which line your cursor is on
   vim.o.cursorline = true
-
-  -- Minimal number of screen lines to keep above and below the cursor.
   vim.o.scrolloff = 10
-
-  -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
-  -- instead raise a dialog asking if you wish to save the current file(s)
-  -- See `:help 'confirm'`
   vim.o.confirm = true
 
   -- Set folding
   vim.o.foldmethod = 'expr'
   vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-  vim.o.foldlevel = 99 -- start with everything unfolded
+  vim.o.foldlevel = 99
 
-  -- [[ Basic Keymaps ]]
-  --  See `:help vim.keymap.set()`
-
-  -- Clear highlights on search when pressing <Esc> in normal mode
-  --  See `:help hlsearch`
+  -- Keymaps
   vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
-  -- Close current buffer
+  vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Exit insert mode' })
   vim.keymap.set('n', '<leader>bx', '<cmd>bdelete<CR>', { desc = 'Close buffer' })
+  vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic error' })
 
-  -- Diagnostic Config & Keymaps
-  --  See `:help vim.diagnostic.Opts`
   vim.diagnostic.config {
     update_in_insert = false,
     severity_sort = true,
     float = { border = 'rounded', source = 'if_many' },
     underline = { severity = { min = vim.diagnostic.severity.WARN } },
-
-    -- Can switch between these as you prefer
-    virtual_text = true, -- Text shows up at the end of the line
-    virtual_lines = false, -- Text shows up underneath the line, with virtual lines
-
-    -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
+    virtual_text = true,
+    virtual_lines = false,
     jump = {
       on_jump = function(_, bufnr)
         vim.diagnostic.open_float {
@@ -323,43 +262,18 @@ do
   }
 
   vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
-  -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
-  -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
-  -- is not what someone will guess without a bit more experience.
-  --
-  -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
-  -- or just use <C-\><C-n> to exit terminal mode
   vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
-  -- TIP: Disable arrow keys in normal mode
-  -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-  -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-  -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-  -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
-  -- Keybinds to make split navigation easier.
-  --  Use CTRL+<hjkl> to switch between windows
-  --
-  --  See `:help wincmd` for a list of all window commands
   vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
   vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
   vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
   vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-  -- [[ Basic Autocommands ]]
-  --  See `:help lua-guide-autocommands`
-
-  -- Highlight when yanking (copying) text
-  --  Try it with `yap` in normal mode
-  --  See `:help vim.hl.on_yank()`
   vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
     group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
     callback = function() vim.hl.on_yank() end,
   })
-  vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-  vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Exit insert mode' })
 end
 
 -- ============================================================
@@ -409,7 +323,6 @@ local function gh(repo) return 'https://github.com/' .. repo end
 
 -- ============================================================
 -- SECTION 3: UI / CORE UX PLUGINS
--- guess-indent, gitsigns, which-key, colorscheme, todo-comments, mini modules
 -- ============================================================
 do
   vim.pack.add { gh 'NMAC427/guess-indent.nvim' }
@@ -417,6 +330,7 @@ do
 
   if vim.g.have_nerd_font then vim.pack.add { gh 'nvim-tree/nvim-web-devicons' } end
 
+  -- Gitsigns
   vim.pack.add { gh 'lewis6991/gitsigns.nvim' }
   require('gitsigns').setup {
     signs = {
@@ -429,6 +343,71 @@ do
   }
   vim.keymap.set('n', '<leader>gb', '<cmd>Gitsigns blame_line<cr>', { desc = '[G]it [B]lame' })
   vim.keymap.set('n', '<leader>gB', '<cmd>Gitsigns blame<cr>', { desc = '[G]it [B]lame file' })
+
+  -- Dashboard
+  vim.pack.add { gh 'nvim-dashboard/dashboard-nvim' }
+  require('dashboard').setup {
+    theme = 'hyper',
+    config = {
+      week_header = {
+        enable = true,
+      },
+      shortcut = {
+        { desc = '󰊳 Update', group = '@property', action = 'lua vim.pack.update()', key = 'u' },
+        {
+          icon = ' ',
+          icon_hl = '@variable',
+          desc = 'Files',
+          group = 'Label',
+          action = 'Telescope find_files',
+          key = 'f',
+        },
+        {
+          desc = ' Config',
+          group = 'Number',
+          action = 'e ~/.config/nvim/init.lua',
+          key = 'c',
+        },
+        {
+          desc = ' Quit',
+          group = 'DiagnosticError',
+          action = 'qa',
+          key = 'q',
+        },
+      },
+    },
+  }
+
+  -- Noice
+  vim.pack.add {
+    gh 'folke/noice.nvim',
+    gh 'MunifTanjim/nui.nvim',
+    gh 'rcarriga/nvim-notify',
+  }
+  require('noice').setup {
+    lsp = {
+      override = {
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        ['vim.lsp.util.stylize_markdown'] = true,
+        ['cmp.entry.get_documentation'] = true,
+      },
+    },
+    presets = {
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
+    },
+  }
+
+  -- Illuminate - highlight word under cursor
+  vim.pack.add { gh 'RRethy/vim-illuminate' }
+  require('illuminate').configure()
+
+  -- Trouble - better diagnostics panel
+  vim.pack.add { gh 'folke/trouble.nvim' }
+  require('trouble').setup()
+  vim.keymap.set('n', '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>', { desc = 'Trouble diagnostics' })
+  vim.keymap.set('n', '<leader>xf', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', { desc = 'Trouble buffer diagnostics' })
 
   -- Harpoon - navigate across up to 4 different files
   vim.pack.add { gh 'nvim-lua/plenary.nvim', { src = gh 'ThePrimeagen/harpoon', version = 'harpoon2' } }
@@ -450,6 +429,7 @@ do
   vim.pack.add { gh 'MeanderingProgrammer/render-markdown.nvim' }
   require('render-markdown').setup {}
 
+  -- Which-key
   vim.pack.add { gh 'folke/which-key.nvim' }
   require('which-key').setup {
     delay = 0,
@@ -466,17 +446,34 @@ do
   vim.pack.add { gh 'folke/tokyonight.nvim' }
   require('tokyonight').setup {
     style = 'night',
+    transparent = true,
     styles = {
       comments = { italic = false },
+      sidebars = 'transparent',
+      floats = 'transparent',
     },
     on_colors = function() end,
     on_highlights = function() end,
   }
   vim.cmd 'colorscheme tokyonight'
+
+  -- Transparent background so Ghostty opacity shows through
+  -- Must be set AFTER colorscheme loads
+  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none' })
+
   -- Theme switching
   vim.keymap.set('n', '<leader>td', function()
     vim.o.background = 'dark'
     vim.cmd 'colorscheme tokyonight'
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none' })
     require('lualine').setup { options = { theme = 'tokyonight' } }
   end, { desc = '[T]heme [D]ark' })
 
@@ -486,9 +483,11 @@ do
     require('lualine').setup { options = { theme = 'tokyonight-day' } }
   end, { desc = '[T]heme [L]ight' })
 
+  -- Todo comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
   require('todo-comments').setup { signs = false }
 
+  -- Mini.nvim
   vim.pack.add { gh 'nvim-mini/mini.nvim' }
 
   require('mini.ai').setup {
@@ -500,17 +499,20 @@ do
   }
 
   require('mini.surround').setup()
+  require('mini.comment').setup()
 
   -- Lualine statusline
   vim.pack.add { gh 'nvim-lualine/lualine.nvim' }
   require('lualine').setup {
     options = { theme = 'tokyonight' },
+    sections = {
+      lualine_c = { { 'filename', path = 1 } },
+    },
   }
 end
 
 -- ============================================================
 -- SECTION 4: SEARCH & NAVIGATION
--- Telescope setup, keymaps, LSP picker mappings
 -- ============================================================
 do
   ---@type (string|vim.pack.Spec)[]
@@ -587,7 +589,6 @@ end
 
 -- ============================================================
 -- SECTION 5: LSP
--- LSP keymaps, server configuration, Mason tools installations
 -- ============================================================
 do
   vim.pack.add { gh 'j-hui/fidget.nvim' }
@@ -695,7 +696,6 @@ end
 
 -- ============================================================
 -- SECTION 6: FORMATTING
--- conform.nvim setup and keymap
 -- ============================================================
 do
   vim.pack.add { gh 'stevearc/conform.nvim' }
@@ -726,7 +726,6 @@ end
 
 -- ============================================================
 -- SECTION 7: AUTOCOMPLETE & SNIPPETS
--- blink.cmp and luasnip setup
 -- ============================================================
 do
   vim.pack.add { { src = gh 'L3MON4D3/LuaSnip', version = vim.version.range '2.*' } }
@@ -735,32 +734,25 @@ do
   vim.pack.add { { src = gh 'saghen/blink.cmp', version = vim.version.range '1.*' } }
   require('blink.cmp').setup {
     keymap = {
-      preset = 'default',
+      preset = 'enter',
     },
-
     appearance = {
       nerd_font_variant = 'mono',
     },
-
     completion = {
       documentation = { auto_show = false, auto_show_delay_ms = 500 },
     },
-
     sources = {
       default = { 'lsp', 'path', 'snippets' },
     },
-
     snippets = { preset = 'luasnip' },
-
     fuzzy = { implementation = 'lua' },
-
     signature = { enabled = true },
   }
 end
 
 -- ============================================================
 -- SECTION 8: TREESITTER
--- Parser installation, syntax highlighting, folds, indentation
 -- ============================================================
 do
   vim.pack.add { { src = gh 'nvim-treesitter/nvim-treesitter', version = 'main' } }
@@ -801,7 +793,6 @@ end
 
 -- ============================================================
 -- SECTION 9: OPTIONAL EXAMPLES / NEXT STEPS
--- kickstart.plugins.* examples
 -- ============================================================
 do
   -- require 'kickstart.plugins.debug'
@@ -809,8 +800,7 @@ do
   require 'kickstart.plugins.lint'
   require 'kickstart.plugins.autopairs'
   require 'kickstart.plugins.neo-tree'
-  -- require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
-
+  -- require 'kickstart.plugins.gitsigns'
   -- require 'custom.plugins'
 end
 
